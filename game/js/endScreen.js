301e74da9f4d1e48987b5095c3867f084c26439c
Ruby you <shih@luluvirus.co.uk>
2015-08-19 09:19:07 +0100

    upload first game

---
+++ game/js/endScreen.js
@@ -0,0 +1,35 @@
+function EndScreen(){
+
+	this.init = function(){
+		
+		var result;
+		var winnerAnnounceText, moneyAnnounceText, winnerVideo;
+		
+		if( Global.betTargetName == Global.winner ){
+			result = "win";
+		}else{
+			result = "lose";
+		}
+
+		// if player win the game
+		switch(result){
+			
+			case "win":
+				winnerAnnounceText = Utility.renderText('Winner is ' + Global.winner + "!!!!", H1, 0, 200, 500, 5);
+				moneyAnnounceText = Utility.renderText('You win ' + Global.BetMoney  + " pounds!", H1, 0, 200, 550, 5);
+				winnerVideo = Utility.renderVideo('assets/'+Global.betTargetName+".mp4", Global.renderer.width, Global.renderer.height*0.8, 10);
+			break;
+			
+			case "lose":
+				winnerAnnounceText = Utility.renderText('Winner is ' + Global.winner + "!!!!", H1, 0, 200, 500, 5);
+				moneyAnnounceText = Utility.renderText('You lose ' + Global.BetMoney  + " pounds!", H1, 0, 200, 550, 5);
+				winnerVideo = Utility.renderVideo('assets/'+Global.againstTargetName+".mp4",Global.renderer.width, Global.renderer.height*0.8, 10);
+			break;
+		};
+
+		Global.stage.addChild(winnerVideo);
+		Utility.addChildToStage([winnerAnnounceText, moneyAnnounceText]);
+
+	};
+
+};
