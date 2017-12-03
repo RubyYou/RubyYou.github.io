96bf6145fc8b22b467804ee9e3bd512f2e79f32b
Ruby you <shih@luluvirus.co.uk>
2017-12-03 19:26:40 +0000

    integrate with firebase backend

--- carehub-demo/js/main.js
+++ carehub-demo/js/main.js
@@ -2,36 +2,75 @@ var materialList = [];
 var careHubList = [];
 
 function init() {
-  Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/11HXw1ECF-phG0ocIK_IzBqvWhc6UxAQt1eFaMocV7aI/pubhtml',
-                   callback: function(data, tabletop) { 
-                       //console.log(data);
-                       materialList = data.materials.elements;
-                       careHubList = data["carehub-list"].elements;
-                       addCareHubList(careHubList);
-                       addMaterialList(materialList);
-                       
-                   }} )
+	var config = {
+		apiKey: "AIzaSyABzMG21mytB-OW1mv37Rfh4tdpvSLaunQ",
+		authDomain: "torrid-inferno-9944.firebaseapp.com",
+		databaseURL: "https://torrid-inferno-9944.firebaseio.com",
+		projectId: "torrid-inferno-9944",
+		storageBucket: "torrid-inferno-9944.appspot.com",
+		messagingSenderId: "836178416951"
+	};
+	firebase.initializeApp(config);
+	firebase.database().ref("carehub").on('value', function (snapshots)
+	{
+		snapshots.forEach( function (snap) {
+			var data = Object.assign ({}, snap.val(), {key: snap.key});
+			careHubList.push (data);
+		});
+	});
+
+	firebase.database().ref("material").on('value', function (snapshots)
+	{
+		snapshots.forEach( function (snap) {
+			var data = Object.assign ({}, snap.val(), {key: snap.key});
+			materialList.push (data);
+		});
+
+	});
+
+	console.log ('careHubList', careHubList);
+	console.log ('materialList', materialList);
+	setTimeout(function (){
+		if ($('.carehub-list').length > 0 ) {
+			addCareHubList (careHubList);
+		}
+
+		if($('#content').length > 0 )
+		{
+			populateContentData ();
+		}
+
+		if ($('.material-list').length > 0)
+		{
+			addMaterialList (materialList);
+		}
+
+
+		if($('#material-content').length > 0 )
+		{
+			populateMaterialContentData();
+		}
+	}, 2000);
 
 }
 
-function addCareHubList(elements){
+
+function addCareHubList (elements)
+{
 
 	for(var i =0; i < elements.length; i++){
 		var progress = getProgressIcon(elements[i].progress);
-		var item = '<a class="item" href=\"content.html#'+ elements[i].id +'\">' + 
-						'<h3>' + elements[i].title + '</h3>' + 
-						'<div class="icon"><i class=\"fa '+ progress.icon +'\"/><p>' + progress.text +'</p></div>' + 
-						'<div class="sub-item">' + 
-							'<span>' + elements[i].type + '</span>' +
-							'<span><i class="fa fa-clock-o" />' + elements[i].time + '</span>' +
-							'<span><i class="fa fa-paper-plane" />' + elements[i].assign + '</span>' + 
-						'</div>' +
-					'</a>';
+		var item = '<a class="item" href=\"content.html#'+ elements[i].key +'\">' +
+			'<h3>' + elements[i].title + '</h3>' +
+			'<div class="icon"><i class=\"fa '+ progress.icon +'\"/><p>' + progress.text +'</p></div>' +
+			'<div class="sub-item">' +
+			'<span>' + elements[i].type + '</span>' +
+			'<span><i class="fa fa-clock-o" />' + elements[i].endTime + '</span>' +
+			'<span><i class="fa fa-paper-plane" />' + elements[i].assignee + '</span>' +
+			'</div>' +
+			'</a>';
 		$('.carehub-list').append(item);
 	}
-
-	populateContentData();
-	toggleSignArea();
 }
 
 function toggleSignArea(){
@@ -44,29 +83,25 @@ function toggleSignArea(){
 	})
 }
 
-
 function populateContentData(){
-	
-	// content page exist
-	if($('#content').length > 0 ){
-		var id = window.location.hash.substr(1);
-		var content = careHubList.filter(function(item){
-			return id == parseInt(item.id);
-		});
-		var progressClass = getProgressClass(content[0].progress);
-		$('#content').find('.progress').find(progressClass).addClass('active');
-		$('#content').find('.title').append(content[0].title);
-		$('#content').find('.type').append(content[0].type);
-		$('#content').find('.endTime').append(content[0].endTime);
-		$('#content').find('.details').append(content[0].details);
-		$('#content').find('.created').append(content[0].created);
-		$('#content').find('.supervisor').append(content[0].supervisor);
-		$('#content').find('.assign').append(content[0].assign);
-		$('#content').find('.lastUpdated').append(content[0].lastUpdated);
-		$('#content').find('.map').attr('src', content[0].map); // replace to proper map
-		$('#content').find('.totalTime').append(content[0].totalTime);
-		$('#content').find('.cost').append(content[0].cost);
-	}
+
+	var key = window.location.hash.substr(1);
+	var content = careHubList.filter(function(item){
+		return key == item.key;
+	});
+	var progressClass = getProgressClass(content[0].progress);
+	console.log(progressClass);
+	$('#content').find('.progress').find(progressClass).addClass('active');
+	$('#content').find('.title').append(content[0].title);
+	$('#content').find('.type').append(content[0].type);
+	$('#content').find('.endTime').append(content[0].endTime);
+	$('#content').find('.details').append(content[0].details);
+	$('#content').find('.supervisor').append(content[0].supervisor);
+	$('#content').find('.assign').append(content[0].assignee);
+	$('#content').find('.lastUpdated').append(content[0].lastUpdated);
+	$('#content').find('.totalTime').append(content[0].hour + 'hours ' + content[0].minus + 'minus');
+	$('#content').find('.cost').append(content[0].cost);
+	toggleSignArea();
 }
 
 function getProgressClass(name){
@@ -75,16 +110,16 @@ function getProgressClass(name){
 	switch (name){
 		case "start":
 			className = ".start";
-		break;
+			break;
 		case "pause":
 			className = ".pause";
-		break;
+			break;
 		case "in progress":
 			className = ".inProgress";
-		break;
+			break;
 		case "resolve":
 			className = ".resolve";
-		break;
+			break;
 	}
 
 	return className;
@@ -93,34 +128,32 @@ function getProgressClass(name){
 function addMaterialList(elements){
 
 	for(var i =0; i < elements.length; i++){
-		var item = '<a class="item" href=\"material-content.html#'+ elements[i].number +'\">' + 
-						'<h4>' + elements[i].number +  '<span class="name">' + elements[i].item  +'</span></h4>' + 
-						'<p>' + elements[i].standard +'</p>' + 
-						'<div class="amount">' + 
-							'<p>數量</p>' +'<p>' + elements[i].storage + '</p>' +
-						'</div>' +
-					'</a>';
+		var item = '<a class="item" href=\"material-content.html#'+ elements[i].key +'\">' +
+			'<h4>' + elements[i].number +  '<span class="name">' + elements[i].item  +'</span></h4>' +
+			'<p>' + elements[i].standard +'</p>' +
+			'<div class="amount">' +
+			'<p>數量</p>' +'<p>' + elements[i].storage + '</p>' +
+			'</div>' +
+			'</a>';
 		$('.material-list').append(item);
 	}
 
-	populateMaterialContentData();
 }
 
 function populateMaterialContentData(){
-	if($('#material-content').length > 0 ){
-		var id = window.location.hash.substr(1);
-		var content = materialList.filter(function(item){
-			return id == item.number;
-		});
-		
-		$('#material-content').find('.standard input').val(content[0].standard);
-		$('#material-content').find('.describe input').val(content[0].describe);
-		$('#material-content').find('.price input').val(content[0].price);
-		$('#material-content').find('.storage input').val(content[0].storage);
-		$('#material-content').find('.require input').val(content[0].require);
-		$('#material-content').find('.date input').val(content[0].date);
-		$('#material-content').find('.number').append('#' + content[0].number);
-	}
+
+	var key = window.location.hash.substr(1);
+	var content = materialList.filter(function(item){
+		return key == item.key;
+	});
+
+	$('#material-content').find('.standard input').val(content[0].standard);
+	$('#material-content').find('.describe input').val(content[0].describe);
+	$('#material-content').find('.price input').val(content[0].price);
+	$('#material-content').find('.storage input').val(content[0].storage);
+	$('#material-content').find('.require input').val(content[0].require);
+	$('#material-content').find('.date input').val(content[0].date);
+	$('#material-content').find('.number').append('#' + content[0].number);
 }
 
 
@@ -131,19 +164,19 @@ function getProgressIcon(progress){
 		case "start":
 			text = "開始";
 			icon = "fa-unlock-alt";
-		break;
+			break;
 		case "pause":
 			text = "暫停";
 			icon = "fa-pause";
-		break;
+			break;
 		case "in progress":
 			text = "處理中";
 			icon = "fa-spinner";
-		break;
-		case "pause":
+			break;
+		case "resolve":
 			text = "解決";
 			icon = "fa-smile-o";
-		break;
+			break;
 	}
 	return { text: text, icon: icon};
 }
